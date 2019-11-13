b7e770c600ad29dbc31668c802c21924b55faf6f
NoraOK <n.okeeffe35@gmail.com>
2019-11-12 23:07:42 -0800

    Ninja Gold Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ninjaGold';
+  gold = 0;
+  activityLog =[];
+
+  addGold(num){
+    this.gold +=num;
+  }
+  addFarmGold(){
+    var num = Math.floor(Math.random()*3)+2;
+    console.log("this is the num"+num);
+    this.addGold(num);
+    this.activityLog.unshift("You earned "+num+" gold from the Farm.")
+  }
+  addCaveGold(){
+    var num = Math.floor(Math.random()*5)+5;
+    this.addGold(num);
+    this.activityLog.unshift("You earned "+num+" gold from the Cave.")
+  }
+  addHouseGold(){
+    var num = Math.floor(Math.random()*8)+7;
+    this.addGold(num);
+    this.activityLog.unshift("You earned "+num+" gold from the House.")
+  }
+  addCasinoGold(){
+    var num = Math.floor(Math.random()*200)-100;
+    this.addGold(num);
+    if(num<0){
+      this.activityLog.unshift("You are a loser. You lost "+num+" gold at the Casino.")
+    }else{
+    this.activityLog.unshift("You earned "+num+" gold from the Casino.")
+  }}
+  resetGold(){
+    this.gold=0;
+  }
+
+  constructor(private _httpService: HttpService) { }
+
+
 }
